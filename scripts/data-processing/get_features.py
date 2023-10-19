8f8c4c93b20e04db68ddebe7361362aedc47cde5
ml-srv-admin <daryamishanina@mail.ru>
2023-10-19 19:53:16 +0500

    file get_features was filled

---
+++ scripts/data-processing/get_features.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import io
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython3 get_features.py data-file\n")
+    sys.exit(1)
+
+f_input = sys.argv[1]
+f_output = os.path.join("data", "stage1", "train.csv")
+os.makedirs(os.path.join("data", "stage1"), exist_ok=True)
+
+def process_data(fd_in, fd_out):
+    fd_in.readline()
+    for line in fd_in:
+        line = line.rstrip('\n').split(',')
+        p_age = line[0]
+        p_education = line[4]
+        p_rase = line[8]
+        p_sex = line[9]
+        p_hourse = line[12]
+        p_salary = line[14]
+
+        fd_out.write("{},{},{},{}\n".format(p_age, p_education, p_rase, p_sex, p_hourse, p_salary))
+
+with io.open(f_input, encoding="utf8") as fd_in:
+    with io.open(f_output, "w", encoding="utf8") as fd_out:
+        process_data(fd_in, fd_out)
